0fdc68eb237e5397842d7fea9840b0e46607e190
anyanar <91902812+anyanar@users.noreply.github.com>
2022-01-10 17:53:31 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+
+
+// let stor =prompt(`Напишите длину стороны`)
+// let per = stor*4
+// alert (`Дана сторона квадрата ${stor}. Найти его периметр ${per} ` )
+
+// let stor =prompt(`Напишите длину стороны`)
+// let plo =stor*stor
+// alert (`Дана сторона квадрата ${stor}. Найти его площадь ${plo}`  )
+
+// let stor1 =prompt(`Напишите длину стороны`)
+// let stor2 =prompt(`Напишите длину стороны`)
+// let plo =stor1*stor2
+// let per =2*(1*stor1+1*stor2)
+// alert(`Даны стороны прямоугольника ${stor1} ${stor2} . Найти его площадь ${plo} и периметр ${per} `)
+
+// let dia =prompt(`Укажите диаметр`)
+// let dli = 3.14*dia
+// alert (`Дан диаметр окружности ${dia}. Найти ее длину ${dli}  `)
+
+//  let dli = prompt(`Укажите длину ребра`)
+//  let ob = dli*dli*dli
+//  let plo = 6*dli*dli
+//  alert (`Дана длина ребра куба ${dli}. Найти объем куба ${ob} и площадь его поверхности ${plo} `)
+
+// let dli1 = prompt(`Укажите длину ребра`)
+// let dli2 = prompt(`Укажите длину ребра`)
+// let dli3 = prompt(`Укажите длину ребра`)
+// let ob = dli1*dli2*dli3
+// let plo = 2*((dli1*dli2)*1 + (dli2*dli3)*1 + (dli1*dli3)*1)
+// alert (`Даны длины ребер ${dli1} ${dli2} ${dli3}. Найти объем куба ${ob} и площадь его поверхности ${plo} `)
+
+// let ra=prompt(`Задайте радиус`)
+// let dli= 2*3.14*ra
+// let plo= 3.14*ra*ra
+// alert (`Найти длину окружности ${dli} и площадь круга ${plo} заданого радиуса ${ra}`)
+
+// let chi1= prompt(`Задайте число`)
+// let chi2= prompt(`Задайте число`)
+// let ari= (chi1*1+chi2*1)/2
+// alert (`Даны 2 числа ${chi1} ${chi2}. Найти их среднее арифметическое ${ari} `)
+
+// let chi1= prompt(`Задайте число`)
+// let chi2= prompt(`Задайте число`)
+// let geo= Math.sqrt(chi1*1+chi2*1)
+// alert (`Даны 2 неотрицательных числа ${chi1} ${chi2}. Найти их среднее геометрическое ${geo} `)
+
+// let chi1= prompt(`Задайте число`)
+// let chi2= prompt(`Задайте число`)
+// let sum= chi1*chi1+chi2*chi2
+// let razn= chi1*chi1-chi2*chi2
+// let pro= chi1*chi2
+// let cha= chi1*chi1/chi2*chi2
+// alert (`Даны 2 ненулевых числа ${chi1} ${chi2}. Найти их сумму ${sum} разность ${razn} произведение ${pro} и частное их квадратов ${cha} `)
+
+// let chi1= prompt(`Задайте число`)
+// let chi2= prompt(`Задайте число`)
+// let sum= Math.abs(chi1)*+Math.abs(chi2)
+// let razn= Math.abs(chi1)*Math.abs(chi2)
+// let pro= Math.abs(chi1)*Math.abs(chi2)
+// let cha= Math.abs(chi1)/Math.abs(chi2)
+// alert (`Даны 2 ненулевых числа ${chi1} ${chi2}. Найти их сумму ${sum} разность ${razn} произведение ${pro} и частное их квадратов ${cha} `)
+
